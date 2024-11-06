5d718546a441f43c13aa34c07f7c85fff8fd80bb
Alex <PersonalXela31@protonmail.com>
2024-11-06 20:45:14 +0000

    feat: added performance metric logging

--- short-test.sh
+++ short-test.sh
@@ -1,2 +1,4 @@
-stress-ng --cpu 32 --cpu-method all --verify -t 20m & 
+stress-ng --cpu 32 --cpu-method all --verify -t 20m --perf > test.txt &
+# might also do some --perf to get performance benchmarks out
+
 turbostat --Summary --show Busy,Bzy_MHz,PkgWatt,PkgTemp --interval 5
